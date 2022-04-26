e343db80e5602d1573d06ee15fd8c0f0fed61b27
amigin <amigin@gmail.com>
2022-04-26 16:33:49 +0300

    Added bulk insert builders

---
+++ src/bulk_insert_builder.rs
@@ -0,0 +1,76 @@
+use crate::sql_line_builder::SqlLineBuilder;
+
+pub struct BulkInsertBuilder<'s> {
+    fields: SqlLineBuilder,
+    values: Vec<SqlLineBuilder>,
+    value_no: u32,
+    current_value: Option<SqlLineBuilder>,
+    values_data: Vec<&'s (dyn tokio_postgres::types::ToSql + Sync)>,
+}
+
+impl<'s> BulkInsertBuilder<'s> {
+    pub fn new() -> Self {
+        Self {
+            fields: SqlLineBuilder::new(",".to_string()),
+            values: Vec::new(),
+            current_value: None,
+            values_data: Vec::new(),
+            value_no: 0,
+        }
+    }
+
+    pub fn append_field(&mut self, field_name: &str) {
+        self.fields.add(field_name)
+    }
+
+    pub fn start_new_value_line(&mut self) {
+        let mut new_line = Some(SqlLineBuilder::new(",".to_string()));
+
+        std::mem::swap(&mut self.current_value, &mut new_line);
+
+        if let Some(new_line) = new_line {
+            self.values.push(new_line);
+        }
+    }
+
+    pub fn append_value(&mut self, value: &'s (dyn tokio_postgres::types::ToSql + Sync)) {
+        if let Some(current_value) = &mut self.current_value {
+            current_value.add(format!("${}", self.value_no).as_str());
+            self.values_data.push(value);
+            self.value_no += 1;
+        } else {
+            panic!("Current value is not set");
+        }
+    }
+
+    pub fn append_value_raw(&mut self, value: &str) {
+        if let Some(current_value) = &mut self.current_value {
+            current_value.add(format!("'{}'", value).as_str());
+        } else {
+            panic!("Current value is not set");
+        }
+    }
+
+    pub fn build(&mut self, table_name: &str) -> String {
+        let mut new_line = None;
+
+        std::mem::swap(&mut self.current_value, &mut new_line);
+
+        let mut result = format!(
+            "INSERT INTO {table_name} ({fields}) VALUES ",
+            fields = self.fields.as_str(),
+        );
+
+        let no = 0;
+        for value in &self.values {
+            if no > 0 {
+                result.push(',');
+            }
+            result.push('(');
+            result.push_str(value.as_str());
+            result.push(')');
+        }
+
+        result
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+mod bulk_insert_builder;
 mod insert_builder;
 mod insert_or_update;
 mod numbered_params;
 mod sql_line_builder;
 mod update_builder;
+pub use bulk_insert_builder::BulkInsertBuilder;
 pub use insert_builder::PosrgresInsertBuilder;
 pub use insert_or_update::PosrgresInsertOrUpdateBuilder;
 pub use numbered_params::NumberedParams;
